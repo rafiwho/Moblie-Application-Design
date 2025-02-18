f9484930dd4be2088da5b841957f333d73b7bb92
rafiwho <uodoyhossanrafi420@gmail.com>
2025-02-18 14:24:32 +0600

    final update with padding,margin,bg separation

--- lib/main.dart
+++ lib/main.dart
@@ -37,52 +37,90 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Container(
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.all(16),
+          width: 350,
+          height: 350,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                spreadRadius: 2,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,  
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text(
-                    '50\% OFF',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Icon(
-                    Icons.favorite,
-                    color:Color.fromARGB(255, 255, 0, 0),
-                  )
-                ],
-              ),
-              const SizedBox(height: 20),
-
-              Image.network(
-                'https://tinyurl.com/cse414-nike-res', 
-                height: 300,
-                width: 300,
-              ),
-              const SizedBox(height: 20),
-
-              const Text(
-                'Nike Air Shoes',
-                style: TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 20),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text(
-                    '\$45,000  ',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                    '\$55,000',
-                    style: TextStyle(
-                      fontSize: 16,
-                      decoration: TextDecoration.lineThrough,
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      '50% OFF',
+                      style: TextStyle(fontSize: 20),
                     ),
-                  ),
-                ],
+                    Icon(
+                      Icons.favorite,
+                      color: Color.fromARGB(255, 255, 0, 0),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  'https://tinyurl.com/cse414-nike-res',
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  'Nike Air Shoes',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      '\$45,000',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      '\$55,000',
+                      style: TextStyle(
+                        fontSize: 16,
+                        decoration: TextDecoration.lineThrough,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
