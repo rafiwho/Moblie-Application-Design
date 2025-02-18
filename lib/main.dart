a8ae752571c1471fb7f2703212125ebeee9fcebb
rafiwho <uodoyhossanrafi420@gmail.com>
2025-02-18 10:35:46 +0600

    using space between

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
   children: [
     Icon(
       Icons.star,
